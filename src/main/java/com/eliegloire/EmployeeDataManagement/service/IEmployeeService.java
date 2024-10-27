d7215b0c202d620c3c22cb3e9f37e45554f3af42
elie <inahon.elie17@yahoo.com>
2024-10-27 12:37:27 -0400

    Spring boot application/ create interface EmployeeService and a class to implement that interface

---
+++ src/main/java/com/eliegloire/EmployeeDataManagement/service/IEmployeeService.java
@@ -0,0 +1,4 @@
+package com.eliegloire.EmployeeDataManagement.service;
+
+public interface IEmployeeService {
+}
---
+++ src/main/java/com/eliegloire/EmployeeDataManagement/service/impl/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.eliegloire.EmployeeDataManagement.service.impl;
+
+import com.eliegloire.EmployeeDataManagement.service.IEmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService implements IEmployeeService {
+}
