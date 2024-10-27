c75d0d89a2136094cf4565add30e3cfbecb5f876
elie <inahon.elie17@yahoo.com>
2024-10-27 12:06:19 -0400

    Spring boot application/create setter and getter, constructor and toString methods

--- src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
+++ src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
@@ -9,10 +9,82 @@ import org.springframework.data.annotation.Id;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
-    String name;
-    String department;
-    String jobTitle;
-    Long salary;
-    Long hireDate;
+    private Long id;
+    private String name;
+    private String department;
+    private String jobTitle;
+    private Long salary;
+    private Long hireDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    public Long getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Long hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Employee(Long id, String name, String department, String jobTitle, Long salary, Long hireDate) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+    public Employee(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                ", hireDate=" + hireDate +
+                '}';
+    }
 }
