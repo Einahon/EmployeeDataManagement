e411c8a5abcd1b1c7137c0c1823a6d91188ba033
elie <inahon.elie17@yahoo.com>
2024-11-06 12:26:20 -0500

    Removed replaced getters and setters...by lombok @

--- src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
+++ src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
@@ -5,12 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
-import lombok.Builder;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 @Builder
    public class Employee{
     @Id
@@ -26,78 +31,7 @@ import java.time.LocalDate;
        @Min(value = 100000, message = "Salary must be a minimum of 100000")
        @Max(value = 999999, message = "Salary must be a maximum of 999999")
        private BigDecimal salary;
-
        private LocalDate hireDate;
 
-       public Long getId() {
-           return id;
-       }
 
-       public void setId(Long id) {
-           this.id = id;
-       }
-
-       public String getName() {
-           return name;
-       }
-
-       public void setName(String name) {
-           this.name = name;
-       }
-
-       public String getDepartment() {
-           return department;
-       }
-
-       public void setDepartment(String department) {
-           this.department = department;
-       }
-
-       public String getJobTitle() {
-           return jobTitle;
-       }
-
-       public void setJobTitle(String jobTitle) {
-           this.jobTitle = jobTitle;
-       }
-
-       public @NotNull(message = "Salary is required") @Min(value = 100000, message = "Salary must be a minimum of 100000") @Max(value = 999999, message = "Salary must be a maximum of 999999") BigDecimal getSalary() {
-           return salary;
-       }
-
-       public void setSalary(BigDecimal salary) {
-           this.salary = salary;
-       }
-
-       public LocalDate getHireDate() {
-           return hireDate;
-       }
-
-       public void setHireDate(LocalDate hireDate) {
-           this.hireDate = hireDate;
-       }
-
-       public Employee(Long id, String name, String department, String jobTitle, BigDecimal salary, LocalDate hireDate) {
-           this.id = id;
-           this.name = name;
-           this.department = department;
-           this.jobTitle = jobTitle;
-           this.salary = salary;
-           this.hireDate = hireDate;
-       }
-       public Employee(){
-
-       }
-
-       @Override
-       public String toString() {
-           return "Employee{" +
-                   "id=" + id +
-                   ", name='" + name + '\'' +
-                   ", department='" + department + '\'' +
-                   ", jobTitle='" + jobTitle + '\'' +
-                   ", salary=" + salary +
-                   ", hireDate=" + hireDate +
-                   '}';
-       }
    }
