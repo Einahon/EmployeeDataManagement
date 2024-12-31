efe3929a80ffb1009bedde5f3d0661f5a64c5174
elie <inahon.elie17@yahoo.com>
2024-12-31 16:32:38 -0500

    Update on the welcome message

--- src/main/java/com/eliegloire/EmployeeDataManagement/controller/EmployeeController.java
+++ src/main/java/com/eliegloire/EmployeeDataManagement/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
@@ -14,9 +15,12 @@ import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+
+    String welcomeMessage = "Welcome to Employee Management";
     @GetMapping("/")
     public String helloController(){
-        return "Welcome to Employee Data";
+        return welcomeMessage;
     }
 
     @Autowired
