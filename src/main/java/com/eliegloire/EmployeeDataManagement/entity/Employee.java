7febbbfe751dc59fe7ba7731ac60ef6648440a5b
elie <inahon.elie17@yahoo.com>
2025-01-01 21:30:27 -0500

    code formation

--- src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
+++ src/main/java/com/eliegloire/EmployeeDataManagement/entity/Employee.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.math.BigDecimal;
